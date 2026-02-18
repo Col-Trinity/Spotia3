e66cc55c335a49352af81d4267a9f4475ee95072
Gianella Carla Lastra <gianellalastra4@gmail.com>
2026-02-18 13:41:31 -0300

    borro console.log de token

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
     const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
-    console.log(token)
+
     if (!token) {
         return NextResponse.redirect(new URL('/auth/login', req.url));
     }
