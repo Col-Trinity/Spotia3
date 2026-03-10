c7cb0fffb5c8eeb3e718c66e356d512c2043a9a0
Gianella Carla Lastra <gianellalastra4@gmail.com>
2026-03-10 16:53:20 -0300

    agregar archivo generations.ts para manejar la lógica de consulta, creación y actualización de generaciones de la IA

---
+++ src/lib/generations.ts
@@ -0,0 +1,19 @@
+import { db } from "./db";
+import { eq } from "drizzle-orm";
+import { AiResponseSchema } from "../types/ia";
+import { generations } from "../db/schema";
+import { z } from "zod";
+export const getGeneration = async (userId: string) => {
+    const generation = await db.query.generations.findFirst({
+        where: (generations) => eq(generations.userId, userId)
+    });
+    return generation;
+}
+
+export const createGeneration = async (userId: string, newGeneration: z.infer<typeof AiResponseSchema>) => {
+    const generation = await db.insert(generations).values({
+        userId,
+        generation: newGeneration,
+    });
+    return generation;
+};
